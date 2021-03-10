7e85d271f0d69af8b1f9bd78f9d48381c1e577e9
Piero Narciso <pieronarciso988@gmail.com>
2021-03-10 12:41:55 -0500

    Added UI components
    * BurgerMenu (in progress)
    * SkillCard
    * SocialBar

---
+++ src/components/ui/BurgerMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    class="p-1 rounded-full focus:bg-primary-dark transform duration-200 hover:scale-105"
+  >
+    <svg
+      class="w-8 h-8 fill-current text-coolWhite-light"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 6h16M4 12h16M4 18h16"
+      ></path>
+    </svg>
+  </button>
+</template>
---
+++ src/components/ui/SkillCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="relative flex items-center overflow-hidden rounded-lg shadow-md group ease-in-out bg-brown hover:shadow-xl hover:scale-105 transform duration-300 hover:bg-opacity-60"
+  >
+    <div
+      class="absolute flex items-center justify-center w-full h-full opacity-0 group-hover:opacity-70 bg-primary-dark"
+    >
+      <p class="text-2xl font-semibold uppercase text-coolWhite-light">
+        {{ item.name }}
+      </p>
+    </div>
+    <div class="p-4">
+      <img class="object-cover" :src="item.path" :alt="item.name" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  props: {
+    item: {
+      type: Object as PropType<{ name: string; path: string }>,
+      required: true,
+    },
+  },
+  setup() {},
+});
+</script>
