b25830d9f9b62f41e65be7cf6f07bdd0fa6c0165
Piero Narciso <pieronarciso988@gmail.com>
2021-03-10 17:32:05 -0500

    Added Title for section components

---
+++ src/components/ui/TitleSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1
+    class="mt-6 text-4xl font-bold tracking-wider hover:translate-x-2 text-coolWhite transform duration-200 hover:scale-105"
+  >
+    <slot></slot>
+  </h1>
+</template>
