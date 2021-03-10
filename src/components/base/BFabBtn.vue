8cda5b62dd870591826f45640a0a38c1c1813a9a
Piero Narciso <pieronarciso988@gmail.com>
2021-03-10 12:41:22 -0500

    Added Fab Button Icon and Base Button

---
+++ src/components/base/BFabBtn.vue
@@ -0,0 +1,5 @@
+<template>
+  <a v-bind="$attrs" class="inline-flex items-center px-2 py-1 font-medium tracking-wide focus:outline-none bg-primary-light rounded-md text-coolWhite-light hover:bg-primary-dark transform duration-200 hover:scale-105" target="_blank">
+    <slot></slot>
+  </a>
+</template>
---
+++ src/components/base/PBtn.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    v-bind="$attrs"
+    class="px-3 py-2 border-2 rounded text-coolWhite border-coolWhite focus:outline-none"
+  >
+    <slot></slot>
+  </button> 
+</template>
