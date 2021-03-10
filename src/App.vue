9ec029016b53c5ab01f09240efbd21cf1cdceebd
Piero Narciso <pieronarciso988@gmail.com>
2021-03-10 12:39:34 -0500

    Project init with Vite Typescript

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <TheNavbar />
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import TheNavbar from './components/layout/TheNavbar.vue';
+
+export default defineComponent({
+  components: {
+    TheNavbar,
+  },
+  name: 'App',
+})
+</script>
