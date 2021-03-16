57f0614c6d1dbae45a8ec1d68b8b2ad054fe8023
Piero Narciso <pieronarciso988@gmail.com>
2021-03-16 13:20:44 -0500

    Improve padding for desktop screen ContactSection

--- src/components/layout/TheContactMe.vue
+++ src/components/layout/TheContactMe.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="pt-4 bg-primary" v-bind="$attrs">
     <TitleSection> Contact Me </TitleSection>
-    <div class="mt-12 text-lg grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:gap-8">
+    <div class="mt-12 text-lg grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:gap-8 xl:gap-12 2xl:gap-32">
       <div class="lg:col-span-1">
-        <p class="lg:text-2xl md:text-xl text-coolWhite-light">
+        <p class="lg:text-2xl md:text-xl text-coolWhite-light lg:mt-8 xl:mt-12 2xl:mt-20 2xl:px-12">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
