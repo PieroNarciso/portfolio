6f7a534700bb339a93de42bcb486c1d439f5b3a3
Piero Narciso <pieronarciso988@gmail.com>
2021-04-14 12:49:57 -0500

    Jest configuration for Unit Testing
    * Added alias path for src and tests directories
    * Jest for typescript

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'ts',
+    'json',
+    'vue',
+  ],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.vue$': 'vue-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
+}
